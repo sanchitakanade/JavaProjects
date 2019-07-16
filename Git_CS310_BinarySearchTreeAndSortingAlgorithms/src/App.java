8ddfc6ecd2f39fcf45a01828fba662a44d5c9b5c
sanchita kanade <sanchitamystudies@gmail.com>
2019-07-16 01:34:44 -0700

    committing App.java file

--- Git_CS310_BinarySearchTreeAndSortingAlgorithms/src/App.java
+++ Git_CS310_BinarySearchTreeAndSortingAlgorithms/src/App.java
@@ -44,11 +44,12 @@ public class App
         for(int b: a) {
             System.out.println(b);
         }
-
-        insertionSort(a);
+        
+        int[] b ={54,8,40,-23,9,12,13,99,0};
+        insertionSort(b);
         System.out.println("\nArray elements after Insertion sort:");
-        for(int b: a) {
-            System.out.println(b);
+        for(int c: b) {
+            System.out.println(c);
         }
 
         BST tree = new BST();
@@ -58,12 +59,13 @@ public class App
         tree.add(10);
         tree.add(25);
         System.out.println("\n\nBinary Search Tree:");
-        System.out.println("height:"+ tree.height(40));
+        System.out.println("height of element 40:"+ tree.height(40));
         
         // printing all elements of tree by inorder traversal
         List<Integer> list = tree.inOrder();
+        System.out.println("Inorder traversal of the tree:");
         for(int e: list) {
             System.out.println(e);
-        }
+        }      
     }
 }
